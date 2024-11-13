bb82c759c5628a3a40a6a6fdd3a1173311a7cbcb
Ian Hsiao <ianhsiao@IX-MBP2023.local>
2024-11-14 01:39:07 +0800

    Initial commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+# install.sh
+#!/bin/bash
+
+ln -s ~/dotfiles/.zshhrc ~/.zshrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+# Add any other installation steps here
+
+echo "Dotfiles setup complete."
+
